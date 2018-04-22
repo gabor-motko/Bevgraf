02342dae765bfe2685621dbe48634ee65aa1efb7
Wampa842 <wampa842@gmail.com>
2018-04-22 23:05:02 +0200

    finished ColorCube

--- ColorCube/main.cpp
+++ ColorCube/main.cpp
@@ -11,19 +11,31 @@
 #pragma region GLOBALS
 struct
 {
-	GLsizei width = 800;
-	GLsizei height = 600;
-	GLint divider = 400;
+	int width = 800;
+	int height = 600;
+	float divider = 400.0f;
 	vec2 size()
 	{
 		return vec2(this->width, this->height);
 	}
+	bool wireframe = true;
 } win;
 
 struct
 {
-	GLsizei width = 500;
-	GLsizei height = 500;
+	float bottom = 0;
+	float left = 100;
+	vec2 pos()
+	{
+		return vec2(this->left, this->bottom);
+	}
+	void setPos(vec2 xy)
+	{
+		this->left = xy.x;
+		this->bottom = xy.y;
+	}
+	int width = 500;
+	int height = 500;
 	vec2 size()
 	{
 		return vec2(this->width, this->height);
@@ -42,7 +54,8 @@ struct
 	vec3 blue = vec3(0.0f, 0.0f, 1.0f);
 	vec3 gray = vec3(0.35f, 0.35f, 0.35f);
 	vec3 sky = vec3(0.0f, 0.5f, 1.0f);
-	vec3 green = vec3(0.0f, 0.70f, 0.0f);
+	vec3 green = vec3(0.0f, 1.0f, 0.0f);
+	vec3 mediumgreen = vec3(0.0f, 0.70f, 0.0f);
 	vec3 darkgreen = vec3(0.0f, 0.5f, 0.0f);
 
 } colors;
@@ -51,14 +64,23 @@ bool * const keyStates = new bool[256]();
 bool * const keyPreviousStates = new bool[256]();
 bool * const mouseStates = new bool[5]();
 bool * const mousePreviousStates = new bool[5]();
+vec2 mousePos = vec2();
+vec2 relativePos = vec2();
+bool selectedDivider = false;
 
 double delta, currentTime, previousTime;
 float visualize_t = 0.0f;
 
 float projCenter = 5.0f;
+
 class Quad
 {
 public:
+	static vec3 rotation;
+	static mat4 getRotMatrix()
+	{
+		return rotateX(rotation.x) * rotateY(rotation.y) * rotateZ(rotation.z);
+	}
 	vec3 points[4];
 	vec3 color;
 	vec3 normal()
@@ -88,6 +110,8 @@ public:
 	}
 };
 
+vec3 Quad::rotation = vec3();
+
 std::vector<vec3> points;
 std::vector<Quad> faces;
 
@@ -96,41 +120,48 @@ void colorv3(vec3 rgb)
 	glColor3f(rgb.x, rgb.y, rgb.z);
 }
 
-void colorv3a(vec3 rgb, float a)
+void printv2(vec2 v)
 {
-	glColor4f(rgb.x, rgb.y, rgb.z, a);
+	printf("(%.2f, %.2f)\n", v.x, v.y);
 }
 
 #pragma endregion
 
 #pragma region GRAPHICS
 
-mat4 w2v = windowToViewport3(vec2(-2, -2), vec2(4.0, 4.0), vec2((win.width - view.width) / 2.0f, (win.height - view.height) / 2.0f), view.size());
-
 void drawCube(std::vector<Quad> quads, bool useOrtho)
 {
+	mat4 w2v = windowToViewport3(vec2(-2, -2), vec2(4.0, 4.0), view.pos(), view.size());
 	mat4 proj;
 	if (useOrtho)
 	{
-		std::sort(faces.begin(), faces.end(), Quad::compareOrtho);
+		std::sort(quads.begin(), quads.end(), Quad::compareOrtho);
 		proj = ortho();
 	}
 	else
 	{
-		std::sort(faces.begin(), faces.end(), Quad::comparePerspective);
+		std::sort(quads.begin(), quads.end(), Quad::comparePerspective);
 		proj = perspective(projCenter);
 	}
 	vec3 * projected = new vec3[4];
-	for (int i = 0; i < faces.size(); ++i)
+	for (int i = 0; i < quads.size(); ++i)
 	{
-		Quad f = faces[i];
+		Quad f = quads[i];
 		for (int j = 0; j < 4; ++j)
 		{
 			projected[j] = hToIh(w2v * proj * ihToH(f.points[j]));
 		}
-		colorv3(f.color);
-		//glBegin(GL_LINE_LOOP);
-		glBegin(GL_QUADS);
+
+		if (win.wireframe)
+		{
+			colorv3(colors.white);
+			glBegin(GL_LINE_LOOP);
+		}
+		else
+		{
+			colorv3(f.color);
+			glBegin(GL_QUADS);
+		}
 		glVertex2f(projected[0].x, projected[0].y);
 		glVertex2f(projected[1].x, projected[1].y);
 		glVertex2f(projected[2].x, projected[2].y);
@@ -139,8 +170,74 @@ void drawCube(std::vector<Quad> quads, bool useOrtho)
 	}
 }
 
-#pragma endregion
+void drawCubeLocal(std::vector<Quad> quads, bool useOrtho)
+{
+	mat4 rot = Quad::getRotMatrix();
+	mat4 w2v = windowToViewport3(vec2(-2, -2), vec2(4.0, 4.0), view.pos(), view.size());
+	mat4 proj;
+	std::vector<Quad> rotated = std::vector<Quad>(quads);
+	for (int i = 0; i < rotated.size(); ++i)
+	{
+		for (int j = 0; j < 4; ++j)
+		{
+			rotated[i].points[j] = hToIh(rot * ihToH(rotated[i].points[j]));
+		}
+	}
+	if (useOrtho)
+	{
+		std::sort(rotated.begin(), rotated.end(), Quad::compareOrtho);
+		proj = ortho();
+	}
+	else
+	{
+		std::sort(rotated.begin(), rotated.end(), Quad::comparePerspective);
+		proj = perspective(projCenter);
+	}
+	vec3 * projected = new vec3[4];
+	for (int i = 0; i < rotated.size(); ++i)
+	{
+		Quad f = rotated[i];
+		for (int j = 0; j < 4; ++j)
+		{
+			projected[j] = hToIh(w2v * proj * ihToH(f.points[j]));
+		}
 
+		if (win.wireframe)
+		{
+			colorv3(colors.white);
+			glBegin(GL_LINE_LOOP);
+		}
+		else
+		{
+			colorv3(f.color);
+			glBegin(GL_QUADS);
+		}
+		glVertex2f(projected[0].x, projected[0].y);
+		glVertex2f(projected[1].x, projected[1].y);
+		glVertex2f(projected[2].x, projected[2].y);
+		glVertex2f(projected[3].x, projected[3].y);
+		glEnd();
+	}
+}
+
+void drawText(float x, float y, const char * s)
+{
+	glRasterPos2f(x, y);
+	for (int i = 0; s[i] != '\0'; ++i)
+		glutBitmapCharacter(GLUT_BITMAP_8_BY_13, s[i]);
+}
+
+void drawTextOverlay()
+{
+	float left = 30.0f;
+	float top = win.height - 20.0f;
+	std::string str;
+
+	str = "Center = " + std::to_string(projCenter);
+	drawText(left, top, str.c_str());
+}
+
+#pragma endregion
 
 #pragma region INPUT
 
@@ -160,9 +257,13 @@ void keyProcess(int x)
 	{
 		exit(0);
 	}
+	if (keyPress('c'))
+	{
+		win.wireframe = !win.wireframe;
+	}
 	if (keyStates['w'])
 	{
-		mat4 rot = rotateX(degToRad(60 * delta));
+		mat4 rot = rotateX(degToRad(-60 * delta));
 		for (int i = 0; i < faces.size(); ++i)
 		{
 			Quad * f = &faces[i];
@@ -174,7 +275,7 @@ void keyProcess(int x)
 	}
 	if (keyStates['s'])
 	{
-		mat4 rot = rotateX(degToRad(-60 * delta));
+		mat4 rot = rotateX(degToRad(60 * delta));
 		for (int i = 0; i < faces.size(); ++i)
 		{
 			Quad * f = &faces[i];
@@ -240,22 +341,73 @@ void keyProcess(int x)
 	{
 		projCenter -= 5.0f * delta;
 	}
-	if (keyStates[GLUT_KEY_LEFT])
+
+	if (mouseStates[GLUT_LEFT_BUTTON] == GLUT_DOWN)
 	{
-		if (visualize_t > 0.0f)
+		if (selectedDivider)
 		{
-			visualize_t -= 0.5f * delta;
-			if (visualize_t < 0.0f)
-				visualize_t = 0.0f;
+			win.divider = mousePos.x;
+		}
+		else
+		{
+			view.setPos(mousePos + relativePos);
 		}
 	}
-	if (keyStates[GLUT_KEY_RIGHT])
+
+	memcpy(keyPreviousStates, keyStates, 256 * sizeof(bool));
+	memcpy(mousePreviousStates, mouseStates, 5 * sizeof(bool));
+
+	glutPostRedisplay();
+	glutTimerFunc(10, keyProcess, 0);
+}
+void keyProcessLocal(int x)
+{
+	if (keyStates['x'])
 	{
-		if (visualize_t < 1.0f)
+		exit(0);
+	}
+	if (keyStates['w'])
+	{
+		Quad::rotation.x -= degToRad(60 * delta);
+	}
+	if (keyStates['s'])
+	{
+		Quad::rotation.x += degToRad(60 * delta);
+	}
+	if (keyStates['a'])
+	{
+		Quad::rotation.y -= degToRad(60 * delta);
+	}
+	if (keyStates['d'])
+	{
+		Quad::rotation.y += degToRad(60 * delta);
+	}
+	if (keyStates['q'])
+	{
+		Quad::rotation.z -= degToRad(60 * delta);
+	}
+	if (keyStates['e'])
+	{
+		Quad::rotation.z += degToRad(60 * delta);
+	}
+	if (keyStates['r'])
+	{
+		projCenter += 5.0f * delta;
+	}
+	if (keyStates['f'])
+	{
+		projCenter -= 5.0f * delta;
+	}
+
+	if (mouseStates[GLUT_LEFT_BUTTON] == GLUT_DOWN)
+	{
+		if (selectedDivider)
 		{
-			visualize_t += 0.5f * delta;
-			if (visualize_t > 1.0f)
-				visualize_t = 1.0f;
+			win.divider = mousePos.x;
+		}
+		else
+		{
+			view.setPos(mousePos + relativePos);
 		}
 	}
 
@@ -288,16 +440,20 @@ void keyUp(int key, int x, int y)
 
 void mouseButton(int button, int state, int x, int y)
 {
-	mouseStates[button] = !state;
+	mouseStates[button] = state;
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		relativePos = view.pos() - vec2((float)x, (float)win.height - (float)y);
+		printf("relativePos: %f %f\n", relativePos.x, relativePos.y);
+	}
+	printf("%d %d\n", button, state);
+	selectedDivider = (fabs(x - win.divider) < 2.0f);
 }
 
 void mouseMove(int x, int y)
 {
-	vec2 pos = vec2(x, win.height - y);
-	if (mouseStates[GLUT_LEFT_BUTTON] == GLUT_DOWN)
-	{
-		if(fabs())
-	}
+	mousePos.x = (float)x;
+	mousePos.y = (float)(win.height - y);
 }
 
 #pragma endregion
@@ -317,19 +473,26 @@ void display()
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	colorv3(colors.white);
-	drawCube(faces, false);
+	drawCube(faces, true);
 
+	colorv3(colors.sky);
 	glScissor(0.0f, 0.0f, win.divider, win.height);
 	glEnable(GL_SCISSOR_TEST);
 	glClear(GL_COLOR_BUFFER_BIT);
-	colorv3(colors.green);
 	glRectf(0.0f, 0.0f, win.divider, win.height);
 
 	colorv3(colors.white);
-	drawCube(faces, true);
+	drawCube(faces, false);
 
 	glDisable(GL_SCISSOR_TEST);
 
+	colorv3(colors.black);
+	glBegin(GL_LINES);
+	glVertex2f(win.divider, 0.0f);
+	glVertex2f(win.divider, win.height);
+	glEnd();
+
+	drawTextOverlay();
 
 	glutSwapBuffers();
 }
@@ -337,13 +500,17 @@ void display()
 void init()
 {
 	previousTime = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-	glClearColor(colors.black.x, colors.black.y, colors.black.z, 1.0f);
+	vec3 cc = colors.mediumgreen;
+	glClearColor(cc.x, cc.y, cc.z, 1.0f);
 	glMatrixMode(GL_PROJECTION);
 	gluOrtho2D(0, win.width, 0, win.height);
 	glShadeModel(GL_FLAT);
 	glEnable(GL_POINT_SMOOTH);
 	glEnable(GL_BLEND);
 	glPointSize(3.0f);
+	view.left = (win.width - view.width) / 2.0f;
+	view.bottom = (win.height - view.height) / 2.0f;
+	mouseStates[GLUT_LEFT_BUTTON] = GLUT_UP;
 }
 
 void initVectors()
@@ -357,12 +524,12 @@ void initVectors()
 	points.push_back(vec3(-1.0, -1.0, 1.0));	//6
 	points.push_back(vec3(-1.0, -1.0, -1.0));	//7
 
-	faces.push_back(Quad(points[7], points[3], points[1], points[5], colors.blue));
-	faces.push_back(Quad(points[2], points[6], points[4], points[0], colors.red));
-	faces.push_back(Quad(points[3], points[2], points[0], points[1], colors.orange));
-	faces.push_back(Quad(points[6], points[7], points[5], points[4], colors.sky));
-	faces.push_back(Quad(points[5], points[1], points[0], points[4], colors.yellow));
-	faces.push_back(Quad(points[6], points[2], points[3], points[7], colors.darkred));
+	faces.push_back(Quad(points[7], points[3], points[1], points[5], colors.red));
+	faces.push_back(Quad(points[2], points[6], points[4], points[0], colors.green));
+	faces.push_back(Quad(points[3], points[2], points[0], points[1], colors.blue));
+	faces.push_back(Quad(points[6], points[7], points[5], points[4], colors.yellow));
+	faces.push_back(Quad(points[5], points[1], points[0], points[4], colors.orange));
+	faces.push_back(Quad(points[6], points[2], points[3], points[7], colors.darkblue));
 }
 
 int main(int argc, char * argv[])
@@ -384,6 +551,7 @@ int main(int argc, char * argv[])
 	glutSpecialUpFunc(keyUp);
 	glutMouseFunc(mouseButton);
 	glutMotionFunc(mouseMove);
+	glutPassiveMotionFunc(mouseMove);
 
 	glutTimerFunc(10, keyProcess, 0);
 
