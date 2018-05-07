efc4458c391b80b5032a5e9c8ad9bbbab4618dde
Wampa842 <wampa842@gmail.com>
2018-05-07 16:46:32 +0200

    update FinalVase

--- FinalVase/main.cpp
+++ FinalVase/main.cpp
@@ -62,7 +62,7 @@ struct
 		return vec2(this->width, this->height);
 	}
 	float fps;
-	float center;
+	float center = 5.0f;
 } win;
 
 // Viewport
@@ -79,8 +79,8 @@ struct
 		this->left = xy.x;
 		this->bottom = xy.y;
 	}
-	int width = 500;
-	int height = 500;
+	int width = 800;
+	int height = 800;
 	vec2 size()
 	{
 		return vec2(this->width, this->height);
@@ -90,16 +90,16 @@ struct
 struct
 {
 	float height = 0;
-	float radius = 5;
+	float radius = 2;
 	float bearing = 0;
 	mat4 getMatrix()
 	{
-		vec3 eye = vec3(radius * cos(bearing), height, radius * sin(bearing));
+		vec3 eye = vec3(radius * cos(bearing), height, -radius * sin(bearing));
 		vec3 target = vec3();
 		vec3 up = vec3(0, 1, 0);
 
 		vec3 mz = normalize(eye - target);
-		vec3 mx = normalize(cross(eye, mz));
+		vec3 mx = normalize(cross(up, mz));
 		vec3 my = normalize(cross(mz, mx));
 
 		return coordinateTransform(eye, mx, my, mz);
@@ -127,12 +127,32 @@ public:
 		vec3 n = normalize(getNormal());
 		return dot(n, v) > 0;
 	}
+	void print()
+	{
+		printf("Quad: %f, %f, %f | %f, %f, %f | %f, %f, %f | %f, %f, %f\n", points[0].x, points[0].y, points[0].z, points[1].x, points[1].y, points[1].z, points[2].x, points[2].y, points[2].z, points[3].x, points[3].y, points[3].z);
+	}
+	Quad(vec3 p0, vec3 p1, vec3 p2, vec3 p3, vec3 color)
+	{
+		points[0] = p0;
+		points[1] = p1;
+		points[2] = p2;
+		points[3] = p3;
+		this->color = color;
+	}
 	Quad(vec3 p0, vec3 p1, vec3 p2, vec3 p3)
 	{
 		points[0] = p0;
 		points[1] = p1;
 		points[2] = p2;
 		points[3] = p3;
+		color = Quad::defaultColor;
+	}
+	Quad()
+	{
+		points[0] = vec3();
+		points[1] = vec3();
+		points[2] = vec3();
+		points[3] = vec3();
 		color = Quad::defaultColor;
 	}
 
@@ -158,7 +178,7 @@ public:
 		}
 		return c;
 	}
-	vec2 points[4];
+	vec2 points[6];
 	float bernsteinPoly(int i, int n, float t)
 	{
 		return binomialCoefficient(n, i) * pow(t, i) * pow(1.0f - t, n - i);
@@ -166,23 +186,25 @@ public:
 	vec2 point(float t)
 	{
 		vec2 qt = vec2();
-		for (int i = 0; i <= 3; ++i)
+		for (int i = 0; i <= 5; ++i)
 		{
-			float b = bernsteinPoly(i, 3, t);
+			float b = bernsteinPoly(i, 5, t);
 			qt.x += b * points[i].x;
 			qt.y += b * points[i].y;
 		}
 		return qt;
 	}
-	BezierCurve(vec2 p0, vec2 p1, vec2 p2, vec2 p3)
+	BezierCurve(vec2 p0, vec2 p1, vec2 p2, vec2 p3, vec2 p4, vec2 p5)
 	{
 		points[0] = p0;
 		points[1] = p1;
 		points[2] = p2;
 		points[3] = p3;
+		points[4] = p4;
+		points[5] = p5;
 	}
 };
-BezierCurve shape = BezierCurve(vec2(0.3, 0), vec2(1, 0.25f), vec2(0, 0.7), vec2(0.3, 1));
+BezierCurve shape = BezierCurve(vec2(0, 0), vec2(1, 0), vec2(1, 0.7), vec2(0.3, 0.7), vec2(0.3, 1.0), vec2(1, 1));
 
 class Light
 {
@@ -199,11 +221,14 @@ public:
 	}
 	vec3 calculate(vec3 qpos, vec3 nrm, vec3 src)
 	{
-		vec3 l = this->pos - qpos;
+		vec3 p = hToIh(cam.getMatrix() * ihToH(this->pos));
+		vec3 l = p - qpos;
 		float mult = (dot(normalize(nrm), normalize(l)) + 1) / 2.0f;
 		return src * mult;
 	}
-} light;
+};
+
+Light light = Light(vec3(2));
 
 #pragma region GRAPHICS
 
@@ -215,7 +240,6 @@ void drawCurve()
 	for (float t = 0; t <= 1; t += 0.05)
 	{
 		vec2 p = shape.point(t) * 200;
-		printMathObject(p);
 		glVertex2f(p.x, p.y);
 	}
 	glEnd();
@@ -234,6 +258,7 @@ void buildVase()
 	geometry.clear();
 	for (float phi = 0.0f; phi < 2 * pi(); phi += dPhi)
 	{
+		geometry.push_back(Quad(vasePoint(phi, 0), vasePoint(phi + dPhi, 0), vec3(), vec3()));
 		for (float h = 0.0f; h < 1.0f; h += dH)
 		{
 			vec3 p0 = vasePoint(phi, h);
@@ -243,15 +268,74 @@ void buildVase()
 			geometry.push_back(Quad(p0, p1, p2, p3));
 		}
 	}
+	//std::vector<vec3> points = std::vector<vec3>();
+	//points.push_back(vec3(1.0, 1.0, 1.0));		//0
+	//points.push_back(vec3(1.0, 1.0, -1.0));		//1
+	//points.push_back(vec3(1.0, -1.0, 1.0));		//2
+	//points.push_back(vec3(1.0, -1.0, -1.0));	//3
+	//points.push_back(vec3(-1.0, 1.0, 1.0));		//4
+	//points.push_back(vec3(-1.0, 1.0, -1.0));	//5
+	//points.push_back(vec3(-1.0, -1.0, 1.0));	//6
+	//points.push_back(vec3(-1.0, -1.0, -1.0));	//7
+
+	//geometry.push_back(Quad(points[7], points[3], points[1], points[5], color.red));
+	//geometry.push_back(Quad(points[2], points[6], points[4], points[0], color.green));
+	//geometry.push_back(Quad(points[3], points[2], points[0], points[1], color.blue));
+	//geometry.push_back(Quad(points[6], points[7], points[5], points[4], color.yellow));
+	//geometry.push_back(Quad(points[5], points[1], points[0], points[4], color.orange));
+	//geometry.push_back(Quad(points[6], points[2], points[3], points[7], color.darkblue));
 }
 
 void drawGeometry()
 {
-	std::sort(geometry.begin(), geometry.end(), Quad::compare);
+	mat4 worldViewProj = cam.getMatrix();
+	std::vector<Quad> tr = std::vector<Quad>(geometry.size());
 	for (int i = 0; i < geometry.size(); ++i)
 	{
-		Quad q = geometry[i];
+		vec3 p0 = hToIh(worldViewProj * ihToH(geometry[i].points[0]));
+		vec3 p1 = hToIh(worldViewProj * ihToH(geometry[i].points[1]));
+		vec3 p2 = hToIh(worldViewProj * ihToH(geometry[i].points[2]));
+		vec3 p3 = hToIh(worldViewProj * ihToH(geometry[i].points[3]));
+		//tr[i] = Quad(p0, p1, p2, p3, light.calculate(geometry[i].points[0], geometry[i].getNormal(), geometry[i].color));
+		tr[i] = Quad(p0, p1, p2, p3);
 	}
+	std::sort(tr.begin(), tr.end(), Quad::compare);
+	mat4 proj = perspective(win.center);
+	mat4 w2v = windowToViewport3(vec2(-1), vec2(2), view.pos(), view.size());
+
+	for (int i = 0; i < tr.size(); ++i)
+	{
+		Quad q = tr[i];
+		color.v3(light.calculate(q.points[0], (q.isBackfacing() ? (-1 * q.getNormal()) : q.getNormal()), q.color));
+		//color.v3(q.color);
+		glBegin(GL_QUADS);
+		for (int j = 0; j < 4; ++j)
+		{
+			vec3 p = hToIh(w2v * proj * ihToH(q.points[j]));
+			glVertex2f(p.x, p.y);
+		}
+		glEnd();
+	}
+}
+
+void drawText(float x, float y, const char * s)
+{
+	glRasterPos2f(x, y);
+	for (int i = 0; s[i] != '\0'; ++i)
+		glutBitmapCharacter(GLUT_BITMAP_8_BY_13, s[i]);
+}
+
+void drawTextOverlay()
+{
+	float left = 30.0f;
+	float top = win.height - 20.0f;
+	std::string str;
+
+	str = "Framerate: " + std::to_string((int)win.fps);
+	drawText(left, top, str.c_str());
+	top -= 14.0f;
+	str = "C = " + std::to_string(win.center);
+	drawText(left, top, str.c_str());
 }
 
 #pragma endregion
@@ -286,27 +370,35 @@ void inputProcess(int x)
 	}
 	if (keyDown('w'))
 	{
-		cam.radius -= t.delta;
+		cam.radius -= 10 * t.delta;
 	}
 	if (keyDown('s'))
 	{
-		cam.radius += t.delta;
+		cam.radius += 10 * t.delta;
 	}
 	if (keyDown('a'))
 	{
-		cam.bearing += t.delta;
+		cam.bearing += 10 * t.delta;
 	}
 	if (keyDown('d'))
 	{
-		cam.bearing -= t.delta;
+		cam.bearing -= 10 * t.delta;
 	}
 	if (keyDown('r'))
 	{
-		cam.height += t.delta;
+		cam.height += 10* t.delta;
 	}
 	if (keyDown('f'))
 	{
-		cam.height -= t.delta;
+		cam.height -= 10 * t.delta;
+	}
+	if (keyDown('t'))
+	{
+		win.center -= t.delta;
+	}
+	if (keyDown('g'))
+	{
+		win.center += t.delta;
 	}
 
 	memcpy(keyPreviousStates, keyStates, 256 * sizeof(bool));
@@ -329,6 +421,10 @@ void display()
 	color.v3(color.black);
 
 	buildVase();
+	drawCurve();
+	drawGeometry();
+
+	drawTextOverlay();
 
 	glutSwapBuffers();
 }
@@ -347,6 +443,7 @@ void init()
 	view.left = (win.width - view.width) / 2.0f;
 	view.bottom = (win.height - view.height) / 2.0f;
 	//mouseStates[GLUT_LEFT_BUTTON] = GLUT_UP;
+	printf("begin\n");
 }
 
 int main(int argc, char ** argv)
